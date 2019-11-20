133501548a5b0b3a090e1fac5a975c1b3c5fee98
aditisheth12 <aditi.shah3@gmail.com>
2019-11-20 22:57:37 +0530

    Adding project FirstPush

---
+++ FirstPush/src/test/java/all/test/cases/TestClass.java
@@ -0,0 +1,9 @@
+package all.test.cases;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("hellooo pushing code to git");
+		System.out.println("1st commit");
+	}
+
+}
